e937f54d921301adca623da8a3e0bde069f3e0a9
xboom1212 <125908740+xboom1212@users.noreply.github.com>
2023-11-22 16:26:17 -0500

    tedfsfd

--- AutonomousInterface.java
+++ AutonomousInterface.java
@@ -6,6 +6,21 @@ public class AutonomousInterface {
 	// todo: write your code here
 	public void DriveForwards()
 	{
-		//hello
+	  motorLeft = hardwareMap.get(DcMotor.class, "motorLeft");
+      motorRight = hardwareMap.get(DcMotor.class, "motorRight");
+      frontLeft = hardwareMap.get(DcMotor.class, "frontLeft");
+      frontRight = hardwareMap.get(DcMotor.class, "frontRight");
+      color1 = hardwareMap.get(ColorSensor.class, "color1");
+      distance1 = hardwareMap.get(DistanceSensor.class, "distance1");
+      imu = hardwareMap.get(BNO055IMU.class, "imu");
+      // Put initialization blocks here
+      motorLeft.setDirection(DcMotor.Direction.REVERSE);
+      waitForStart();
+      // Put run blocks here
+      motorLeft.setPower(1);
+      motorRight.setPower(1);
+      while (opModeIsActive()) {
+        // Put loop blocks here
+      }
 	}
 }
