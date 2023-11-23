257b40d05f4d907c3d8be2e252baa7280421ee47
Luke <38165719+Planity3@users.noreply.github.com>
2023-11-23 10:14:10 -0500

    Update AutonomousInterface.java

--- AutonomousInterface.java
+++ AutonomousInterface.java
@@ -14,7 +14,7 @@ public class AutonomousInterface {
 		return[-Math.abs(power), -Math.abs(power), -Math.abs(power), -Math.abs(power)];
 	}
 
-public double[] StrafeLeft(float power)
+	public double[] StrafeLeft(float power)
 	{
 		return[-Math.abs(power), Math.abs(power),Math.abs(power), -Math.abs(power)];
 	}
@@ -23,28 +23,28 @@ public double[] StrafeLeft(float power)
 		return[Math.abs(power), -Math.abs(power),-Math.abs(power), Math.abs(power)];
 	}
 
-public double[] DriveNE(float power)
-{
-return[Math.abs(power), 0.0, 0.0, Math.abs(power)];
-}
-public double[] DriveSE(float power)
-{
-return[0, -Math.abs(power), -Math.abs(power), 0.0];
-}
-public double[] DriveSW(float power)
-{
-return[-Math.abs(power), 0.0, 0.0, -Math.abs(power)];
-}
-public double[] DriveNW(float power)
-{
-return[0.0, Math.abs(power), Math.abs(power), 0.0];
-}
-public double[] RotateCW(float power)
-{
-return[Math.abs(power), -Math.abs(power), Math.abs(power), -Math.abs(power)];
-}
-public double[] RotateCCW(float power)
-{
-return[-Math.abs(power), Math.abs(power), -Math.abs(power), Math.abs(power)];
-}
+	public double[] DriveNE(float power)
+	{
+		return[Math.abs(power), 0.0, 0.0, Math.abs(power)];
+	}
+	public double[] DriveSE(float power)
+	{
+		return[0, -Math.abs(power), -Math.abs(power), 0.0];
+	}
+	public double[] DriveSW(float power)
+	{
+		return[-Math.abs(power), 0.0, 0.0, -Math.abs(power)];
+	}
+	public double[] DriveNW(float power)
+	{
+		return[0.0, Math.abs(power), Math.abs(power), 0.0];
+	}
+	public double[] RotateCW(float power)
+	{
+		return[Math.abs(power), -Math.abs(power), Math.abs(power), -Math.abs(power)];
+	}
+	public double[] RotateCCW(float power)
+	{
+		return[-Math.abs(power), Math.abs(power), -Math.abs(power), Math.abs(power)];
+	}
 }
