7e4703ecdd9c8915e69726f499665b10404c9b49
Planity3 <38165719+Planity3@users.noreply.github.com>
2023-12-29 15:23:09 -0500

    Switched project to android studio
    This should allow us to get roadrunner up and running :)

---
+++ OldFiles/AutonomousInterface.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode;
+
+
+public class AutonomousInterface {
+
+	// FL, FR, BL, BR
+	public double[] DriveForwards(float power)
+	{
+		return [Math.abs(power), Math.abs(power), Math.abs(power), Math.abs(power)];
+	}
+
+	public double[] DriveBackwards(float power)
+	{
+		return[-Math.abs(power), -Math.abs(power), -Math.abs(power), -Math.abs(power)];
+	}
+
+	public double[] StrafeLeft(float power)
+	{
+		return[-Math.abs(power), Math.abs(power),Math.abs(power), -Math.abs(power)];
+	}
+	public double[] StrafeRight(float power)
+	{
+		return[Math.abs(power), -Math.abs(power),-Math.abs(power), Math.abs(power)];
+	}
+
+	public double[] DriveNE(float power)
+	{
+		return[Math.abs(power), 0.0, 0.0, Math.abs(power)];
+	}
+	public double[] DriveSE(float power)
+	{
+		return[0, -Math.abs(power), -Math.abs(power), 0.0];
+	}
+	public double[] DriveSW(float power)
+	{
+		return[-Math.abs(power), 0.0, 0.0, -Math.abs(power)];
+	}
+	public double[] DriveNW(float power)
+	{
+		return[0.0, Math.abs(power), Math.abs(power), 0.0];
+	}
+	public double[] RotateCW(float power)
+	{
+		return[Math.abs(power), -Math.abs(power), Math.abs(power), -Math.abs(power)];
+	}
+	public double[] RotateCCW(float power)
+	{
+		return[-Math.abs(power), Math.abs(power), -Math.abs(power), Math.abs(power)];
+	}
+}
